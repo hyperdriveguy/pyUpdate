895f56952b9170ff8d25a92b81e6306d040391fa
hyperdriveguy <hyperdriveguy@users.noreply.github.com>
2015-06-03 12:40:18 -0700

    finish installer

--- install.sh
+++ install.sh
@@ -4,4 +4,17 @@
 sudo apt-get update
 sudo apt-get install python3 git
 
-git clone 
+git clone https://github.com/hyperdriveguy/pyUpdate.git
+cd pyUpdate-master
+sudo mv pyUpdate.py /bin/pyUpdate.py
+sudo mv py-update /bin/py-update
+rm install.sh
+rm readme.md
+cd $HOME
+mkdir .pyUpdate
+cd pyUpdate-master
+mv updaterConfig.txt $HOME/.pyUpdate/updaterConfig.txt
+rm .gitignore
+cd $HOME
+rm pyUpdate-master
+exit
